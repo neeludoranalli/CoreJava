ac1deee5ee11e8561d136636c295491a6b84042b
neeludoranalli <92967525+neeludoranalli@users.noreply.github.com>
2021-12-22 11:21:14 +0530

    type_casting (primitive)

---
+++ typeCasting/src/typecasting/Typecasting.java
@@ -0,0 +1,193 @@
+package typecasting;
+
+public class Typecasting {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Integer values Typecasting");
+		int num=10;
+		System.out.println("The integer value:" + num);
+		
+		byte numberr =(byte) num;
+		System.out.println("The byte value:" + numberr);
+		
+		double data =num;
+		System.out.println("The double value:" + data);
+		
+		float number =num;
+		System.out.println("The float value:" + number);
+		
+		short res =(short) num;
+		System.out.println("The short value:" + res);
+		
+		long result =num;
+		System.out.println("The long value:" + result);
+		
+		char response =(char) num;
+		System.out.println("The character value:" + response);
+		
+		/*
+		  boolean results=(boolean)numb; 
+		 */
+		
+
+		
+
+		System.out.println("floating values Typecasting");
+		float num1=98.0f;
+		System.out.println("The floating value:" + num1);
+		
+		byte numberr1 =(byte) num1;
+		System.out.println("The byte value:" + numberr1);
+		
+		double data1 =num1;
+		System.out.println("The double value:" + data1);
+		
+		int number1 =(int) num1;
+		System.out.println("The float value:" + number1);
+		
+		short res1 =(short) num1;
+		System.out.println("The short value:" + res1);
+		
+		long result1 =(long) num1;
+		System.out.println("The long value:" + result1);
+		
+		char response1 =(char) num1;
+		System.out.println("The character value:" + response1);
+		
+		
+		System.out.println();
+
+		System.out.println("double values Typecasting");
+		double num2=989.0;
+		System.out.println("The floating value:" + num2);
+		
+		byte numberr2= (byte) num2;
+		System.out.println("The byte value:" + numberr1);
+		
+		float data2 = (float) num2;
+		System.out.println("The double value:" + data1);
+		
+		int number2 = (int) num2;
+		System.out.println("The float value:" + number1);
+		
+		short res2 = (short) num2;
+		System.out.println("The short value:" + res1);
+		
+		long result2 = (long) num2;
+		System.out.println("The long value:" + result1);
+		
+		char response2 = (char) num2;
+		System.out.println("The character value:" + response1);
+		
+		
+		System.out.println();
+
+		System.out.println("short values Typecasting");
+		short Num=7;
+		System.out.println("The double value:" + Num);
+		
+		byte numberr3= (byte) Num;
+		System.out.println("The byte value:" + numberr3);
+		
+		float data3= Num;
+		System.out.println("The double value:" + data1);
+		
+		int number3 = Num;
+		System.out.println("The float value:" + number3);
+		
+		double res3 = Num;
+		System.out.println("The double value:" + res1);
+		
+		long result11 = Num;
+		System.out.println("The long value:" + result11);
+		
+		char response22 =(char) Num;
+		System.out.println("The character value:" + response22);
+	
+		
+		System.out.println();
+
+		System.out.println("long values Typecasting");
+		long numb=98345;
+		System.out.println("The floating value:" + numb);
+		
+		byte numberr4= (byte) numb;
+		System.out.println("The byte value:" + numberr4);
+		
+		float data4 = numb;
+		System.out.println("The double value:" + data1);
+		
+		int number4 = (int) numb;
+		System.out.println("The float value:" + number4);
+		
+		short res4 = (short) numb;
+		System.out.println("The short value:" + res4);
+		
+		double result4 = numb;
+		System.out.println("The long value:" + result4);
+		
+		char response4 = (char) numb;
+		System.out.println("The character value:" + response4);
+		
+		
+		
+		System.out.println();
+
+		System.out.println("byte values Typecasting");
+		byte digit=2;
+		System.out.println("The floating value:" + digit);
+		
+		double numberrr= digit;
+		System.out.println("The byte value:" + numberrr);
+		
+		float dataa = digit;
+		System.out.println("The double value:" + dataa);
+		
+		int numberrrr= digit;
+		System.out.println("The float value:" + numberrrr);
+		
+		short ress = digit;
+		System.out.println("The short value:" + ress);
+		
+		long resultt = digit;
+		System.out.println("The long value:" + resultt);
+		
+		char responsee = (char) digit;
+		System.out.println("The character value:" + responsee);
+		
+	
+		
+		System.out.println();
+
+		System.out.println("character values Typecasting");
+		char ch='z';
+		System.out.println("The floating value:" + ch);
+		
+		byte numberr8=  (byte) ch;
+		System.out.println("The byte value:" + numberr8);
+		
+		float data8 = ch;
+		System.out.println("The double value:" + data8);
+		
+		int number8 = ch;
+		System.out.println("The float value:" + number8);
+		
+		short res8= (short) ch;
+		System.out.println("The short value:" + res8);
+		
+		long result8 = ch;
+		System.out.println("The long value:" + result8);
+		
+		double response8= ch;
+		System.out.println("The character value:" + response8);
+		
+	
+       
+		System.out.println();
+
+	
+	
+	}
+
+}
